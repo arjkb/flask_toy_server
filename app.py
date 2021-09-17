89a8092c5e3f4bf1c6f29dc3465e8cdc1e49cda6
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-17 07:42:29 +0530

    simplify the code

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
+from logging import error
 from flask import Flask, request
 
 app = Flask(__name__)
 
+WORD_KEY = 'word'
+
 @app.route('/reverse', methods=['POST'])
 def reverse():
-    try:
-        s = request.json['word']
-    except KeyError:
-        s = None
+    s = request.json[WORD_KEY] if WORD_KEY in request.json else None
 
     if s is None:
-        result = {
-            "error" : "No body given"
-        }, 400
+        result, error_code = {"error" : "No body given"}, 400
     else:
-        result = {
-            "reversed" : s[::-1]
-        }
+        result, error_code = {"reversed" : s[::-1]}, 200
 
-    return result
+    return result, error_code
 
 @app.route('/')
 def hello():
