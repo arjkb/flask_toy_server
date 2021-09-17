044c199528aa731048f7e6213e1da7619c46f4fa
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-17 07:31:34 +0530

    make api to accept a word and reverse it if it exists, or else return an error message with status 400

--- app.py
+++ app.py
@@ -2,6 +2,24 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/reverse', methods=['POST'])
+def reverse():
+    try:
+        s = request.json['word']
+    except KeyError:
+        s = None
+
+    if s is None:
+        result = {
+            "error" : "No body given"
+        }, 400
+    else:
+        result = {
+            "reversed" : s[::-1]
+        }
+
+    return result
+
 @app.route('/')
 def hello():
     return {
